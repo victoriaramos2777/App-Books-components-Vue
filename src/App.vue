363498bc10f5b2e292f6abe452909613025945b2
Victoria Ramos <777vikiramos22@gmail.com>
2023-02-21 17:58:41 -0600

    componentes dinamicos finalizados

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,66 @@
 <script>
 export default {
-    data() {
-        return{
-          
-            books:[
-             {
-                id:1,
-                title: "Dime Quién Soy ",
-                autor: "Julia Navarro",
-                releaseDate: "2010-03-05",
-             },
-           ],
-        };
-    },
-    
+  data() {
+    return  {
+      books: [
+        {
+          id: 1,
+          title: "Dime Quién Soy I",
+          autor: "Julia Navarro",
+          releaseDate: "2010-03-05",
+        },
+        {
+          id: 2,
+          title: "Dime Quién Soy II",
+          autor: "Julia Navarro",
+          releaseDate: "2010-03-05",
+        }, {
+          id: 3,
+          title: "Dime Quién Soy III",
+          autor: "Julia Navarro",
+          releaseDate: "2010-03-05",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <template>
   <h1>Mis Libros</h1>
   <ul>
-    <book-info></book-info>
-    <book-info></book-info>
-    <book-info></book-info>
+    <book-info
+      id="1"
+      title="Dime Quién Soy I "
+      author="Julia Navarro"
+      releaseDate="2010-03-05"
+      is-liked="1"
+    ></book-info>
+    <book-info  id="2"
+      title="Dime Quién Soy II "
+      author="Julia Navarro"
+      releaseDate="2010-03-05"
+      is-liked="0"></book-info>
+    <book-info  id="3"
+      title="Dime Quién Soy III"
+      author="Julia Navarro"
+      releaseDate="2010-03-05"
+      is-liked="1"></book-info>
   </ul>
 </template>
 
 <style>
-h1{
-    color:rgb(238, 238, 130);
-    background-color: black;
-    width: 30%;
-    text-align: center;
+h1 {
+  color: rgb(238, 238, 130);
+  background-color: black;
+  width: 96%;
+  text-align: center;
+  padding: 5%;
 }
-ul{
+ul {
   list-style: none;
   background-color: black;
   color: aqua;
-  width: 30%;
+  padding: 3%;
 }
 </style>
--- src/componets/BookInfo.vue
+++ src/componets/BookInfo.vue
@@ -1,37 +1,70 @@
 <script>
-export default {
-    data() {
-        return{
-            detailsAreVisible:false,
-            book: {
-                id:1,
-                title: "Dime Quién Soy",
-                autor: "Julia Navarro",
-                releaseDate: "2010-03-05",
-             },
-           
-        };
 
+export default {
+ // props:["id", "title","autor", "releaseDate","isLiked"],
+ //objeto que valide los props
+    props:{
+      id: {
+        type: String,
+        required: true,
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+      author: {
+        type: String,
+        required: true,
+      },
+      releaseDate: {
+        type: String,
+        required: true,
+      },
+      isLiked: {
+        type: String,
+        required: false,
+        default:"0",
+        validator:function(value){
+          return value === "1" || value === "0";
+        },
+      },
     },
-    methods:{
-      toggletDetails(){
-       this.detailsAreVisible  = !this.detailsAreVisible;
+  
+  data() {
+    return {
+      detailsAreVisible: false,
+      bookIsLiked:this.isLiked,
+    };
+  },
+  methods: {
+    toggletDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggletFavorite(){
+      if(this.bookIsLiked === "1"){
+            this.bookIsLiked  = "0";
+      }else{
+        this.bookIsLiked = "1";
       }
     }
+  },
 };
 </script>
 
 <template>
-<li>
+  <li>
     <h2>
-        {{ book.title }}
+      {{title }} {{  bookIsLiked==="1" ? "(Liked)":"(Not Liked)" }}
     </h2>
     <button type="button" @click="toggletDetails">
-    {{ detailsAreVisible ? "Hiden": "Show"}}
+      {{ detailsAreVisible ? "Hiden" : "Show" }}
     </button>
-    <ul v-if ="detailsAreVisible">
-        <li><strong>Autor</strong>{{ book.autor }}</li>
-        <li><strong>Release Date</strong>{{ book.releaseDate }}</li>
+    <button type="button" @click="toggletFavorite">
+      Togglet  Favorite
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Autor: </strong>{{ author }}</li>
+      <li><strong>Release Date: </strong>{{releaseDate }}</li>
     </ul>
-</li>
+  </li>
 </template>
